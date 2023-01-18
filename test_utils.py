aa3d66c6498f4f6b7a70068a2d25a1c4d186c761
Manonp59 <manon-platteau@orange.fr>
2023-01-18 15:05:45 +0100

    fusion

--- test_utils.py
+++ test_utils.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import patch
 import random
-from utils import no_one_is_dead, drink_potion, display_scores, create_file, menu, store_scores
+from utils import no_one_is_dead, drink_potion, display_scores, create_file, menu, store_scores, attack, who_won, total_score, principal_menu
 
 class TestWithUnittest(unittest.TestCase):
     
@@ -35,17 +35,12 @@ class TestWithUnittest(unittest.TestCase):
         self.assertTrue(calling_1 == '1' and calling_2 == '2')
         self.assertEqual(menu(0),'1')
         
+        
     def test_store_scores(self):
         store_scores(1,2,3)
         with open('scores.csv','r') as fichier:
             self.assertIn('1' and '2' and '3', fichier.read())
 
-import unittest 
-from unittest.mock import patch
-import random
-from utils import attack, who_won, total_score, principal_menu
-
-class TestAddWithUnittest(unittest.TestCase):
 
     def test_attack(self):
         random.seed(15)
@@ -53,15 +48,18 @@ class TestAddWithUnittest(unittest.TestCase):
         random.seed(15)
         self.assertEqual(attack("Marie",50,50),(50,42))
     
+    
     def test_who_won(self):
         self.assertEqual(who_won(34,0,3,4),(4,4))
         self.assertEqual(who_won(0,17,3,1),(3,2))
+    
     
     def test_total_score(self):
         total_score(3,2)
         path_to_file="scores.csv"
         with open(path_to_file,"r") as fichier:
             self.assertIn("3" and "2",fichier.read())
+    
     
     @patch('builtins.input', side_effect = ['1', '2'])
     def test_principal_menu(self, mock_input):
